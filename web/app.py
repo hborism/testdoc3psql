1d84100af15bd621b31184aa91298fc3f9671de9
hborism <henrik.borismoller@gmail.com>
2016-07-25 18:47:52 +0200

    Flask-sqlalchemy database model complete for scoring. no API for retreiving the data has been inplemented yet, but it is possible to add the data to the database. The model is weak in that it is possible to add scores for holes that do not exist for a specific course. This can be fixed by instead of using flask-sqlalchemy use sqlalchemy, see http://docs.sqlalchemy.org/en/latest/orm/extensions/declarative/table_config.html

--- web/app.py
+++ web/app.py
@@ -1,4 +1,6 @@
 import os
+import uuid
+import datetime
 from flask import Flask
 from flask import request, render_template, jsonify, send_file, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
@@ -18,18 +20,26 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@postgres:5432/postgres'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://{db_user}:{db_password}@{db_service}:{db_port}/{db_name}
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOAD_FOLDER'] = '/usr/src/app/profilepictures'
 
 
 db = SQLAlchemy(app)
 
 
-#-----------------------------DATABASEMODEL------------------------------
+
+'''
+---------------------------------------------------------------------
+ ____        _        _                    __  __           _      _
+|  _ \  __ _| |_ __ _| |__   __ _ ___  ___|  \/  | ___   __| | ___| |
+| | | |/ _` | __/ _` | '_ \ / _` / __|/ _ \ |\/| |/ _ \ / _` |/ _ \ |
+| |_| | (_| | || (_| | |_) | (_| \__ \  __/ |  | | (_) | (_| |  __/ |
+|____/ \__,_|\__\__,_|_.__/ \__,_|___/\___|_|  |_|\___/ \__,_|\___|_|
+---------------------------------------------------------------------
+'''
 
 class Club(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
-    # players = db.relationship('Player', backref='club', lazy='dynamic')
 
     def __init__(self, name):
         self.name = name
@@ -37,76 +47,157 @@ class Club(db.Model):
     def __repr__(self):
         return '<Club %r>' % self.name
 
+
+
 class Player(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    firstName = db.Column(db.String(30))
-    lastName = db.Column(db.String(40))
+    first_name = db.Column(db.String(30))
+    last_name = db.Column(db.String(40))
     email = db.Column(db.String(120), unique=True)
     hcp = db.Column(db.Integer)
-    clubId = db.Column(db.Integer, db.ForeignKey('club.id'))
+    profile_picture_id = db.Column(db.String(36), unique=True)
+    club_id = db.Column(db.Integer, db.ForeignKey('club.id'))
 
-    def __init__(self, firstName, lastName, email, hcp, clubId):
-        self.firstName = firstName
-        self.lastName = lastName
+    def __init__(self, first_name, last_name, email, hcp, profile_picture_id, club_id):
+        self.first_name = first_name
+        self.last_name = last_name
         self.email = email
         self.hcp = hcp
-        self.clubId = clubId
+        self.profile_picture_id = profile_picture_id
+        self.club_id = club_id
 
     def __repr__(self):
         return '<Player %r>' % self.email
 
-# class Course(db.Model):
-# 	id = db.Column(db.Integer, primary_key=True)
-# 	name = db.Column(db.String(40))
-# 	numberOfHoles = db.Column(db.Integer)
-# 	clubId= db.Column(db.Integer, db.ForeignKey(club.id))
-
-# 	def __init__(self, name, numberOfHoles, clubId)
-# 		self.name = name
-# 		self.numberOfHoles = numberOfHoles
-# 		self.clubId = clubId
-
-# 	def __repr__(self):
-# 		return '<Course'
-
-#-------------------------------ROUTING-----------------------------------
 
 
-@app.route('/upload', methods=['GET', 'POST'])
-def upload():
-    # if request.method == 'POST':
-        # file = request.files['file']
-        # extension = os.path.splitext(file.filename)[1]
-        # f_name = str(uuid.uuid4()) + extension
-        # file.save(os.path.join(app.config['UPLOAD_FOLDER'], f_name))
-        # return json.dumps({'filename':f_name})
-    return "use POST request"
+class Course(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(40))
+	number_of_holes = db.Column(db.Integer)
+	club_id = db.Column(db.Integer, db.ForeignKey('club.id'))
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+	def __init__(self, name, number_of_holes, club_id):
+		self.name = name
+		self.number_of_holes = number_of_holes
+		self.club_id = club_id
 
+	def __repr__(self):
+		return '<Course %r>' % self.name
+
+
+
+class Hole(db.Model):
+    tee_no = db.Column(db.Integer, primary_key=True)
+    course_id = db.Column(db.Integer, db.ForeignKey('course.id'), primary_key=True)
+    par = db.Column(db.Integer)
+    index = db.Column(db.Integer)
+    red_tee = db.Column(db.Integer)
+    blue_tee = db.Column(db.Integer)
+    yellow_tee = db.Column(db.Integer)
+    white_tee = db.Column(db.Integer)
+
+    def __init__(self, tee_no, course_id, par, index, red_tee, blue_tee, yellow_tee, white_tee):
+        self.tee_no = tee_no
+        self.course_id = course_id
+        self.par = par
+        self.index = index
+        self.red_tee = red_tee
+        self.blue_tee = blue_tee
+        self.yellow_tee = yellow_tee
+        self.white_tee = white_tee
+
+    def __repr__(self):
+        return '<Hole %r>' % self.tee_no
+
+
+
+class Round(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    course_id = db.Column(db.Integer, db.ForeignKey('course.id'))
+    created_by_player_id = db.Column(db.Integer, db.ForeignKey('player.id'))
+    timestamp = db.Column(db.DateTime, default=datetime.now)
+
+    def __init__(self, course_id, created_by_player_id):
+        self.course_id = course_id
+        self.created_by_player_id = created_by_player_id
+
+    def __repr__(self):
+        return '<Round %r>' % self.id
+
+
+
+class Score(db.Model):
+    round_id = db.Column(db.Integer, db.ForeignKey('round.id'), primary_key=True)
+    player_id = db.Column(db.Integer, db.ForeignKey('player.id'), primary_key=True)
+
+    def __init__(self, round_id, player_id):
+        self.round_id = round_id
+        self.player_id = player_id
+
+
+
+'''CAREFUL HERE: since hole_no is not a foreign key to hole.hole_no and hole.course_id
+it might be possible to add scores to holes that do not exist if not careful'''
+class ScoreHole(db.Model):
+    round_id = db.Column(db.Integer, db.ForeignKey('round.id'), primary_key=True)
+    player_id = db.Column(db.Integer, db.ForeignKey('player.id'), primary_key=True)
+    hole_no = db.Column(db.Integer, primary_key=True)
+    shots = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, round_id, player_id, hole_no, shots):
+        self.round_id = round_id
+        self.player_id = player_id
+        self.hole_no = hole_no
+        self.shots = shots
+
+
+
+'''
+--------------------------------------------
+ ____   ___  _   _ _____ ___ _   _  ____
+|  _ \ / _ \| | | |_   _|_ _| \ | |/ ___|
+| |_) | | | | | | | | |  | ||  \| | |  _
+|  _ <| |_| | |_| | | |  | || |\  | |_| |
+|_| \_\\___/ \___/  |_| |___|_| \_|\____|
+--------------------------------------------
+'''
+
+# @app.route('/upload', methods=['GET', 'POST'])
+# def upload():
+#     # if request.method == 'POST':
+#         # file = request.files['file']
+#         # extension = os.path.splitext(file.filename)[1]
+#         # f_name = str(uuid.uuid4()) + extension
+#         # file.save(os.path.join(app.config['UPLOAD_FOLDER'], f_name))
+#         # return json.dumps({'filename':f_name})
+#     return "use POST request"
+#
+# def allowed_file(filename):
+#     return '.' in filename and \
+#            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+'''
+UPLOAD PICTURE
+work in progress. currently only possible to upload via the html page
+'''
 @app.route('/uploadpic', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-
-    	if ('file' in request.files):
-    		return "true"
-    	return "false"
-
-        # # check if the post request has the file part
-        # if 'file' not in request.files:           
-        #     return 'No file part'
-        # file = request.files['file']
-        # # if user does not select file, browser also
-        # # submit a empty part without filename
-        # if file.filename == '':
-        #     return 'No selected file'
-        # if file and allowed_file(file.filename):
-        #     filename = secure_filename(file.filename)
-        #     file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        #     # return redirect(url_for('upload_file', filename=filename))
-        #     return "uploaded :)"
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            return 'No file part'
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            return 'No selected file'
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            # return redirect(url_for('upload_file', filename=filename))
+            return "uploaded :)"
     return '''
     <!doctype html>
     <title>Upload new File</title>
@@ -117,38 +208,106 @@ def upload_file():
     </form>
     '''
 
-@app.route('/picture/<int:userId>')
-def get_pitcure(userId):
+'''
+GET PICTURE
+get the picture of specific name (including .jpg or .png etc)
+'''
+@app.route('/picture/<pictureId>')
+def get_pitcure(pictureId):
 	try:
-		filename='profilepictures/'+str(userId)+'.jpg'
+		filename='profilepictures/'+str(pictureId)
 		return send_file(filename, mimetype='image')
 	except FileNotFoundError:
 		return jsonify({"error": {"message":"There exist no picture with that id", "type":"not found"}})
 
 
 
-@app.route('/newuser', methods=['POST'])
-def newuser():
-	newplayer=Player(request.json["firstName"], request.json["lastName"],request.json["email"],request.json["hcp"],request.json["clubId"])
+'''NEW CLUB
+is done directly in database, ie no API for this function
+'''
 
-	try: 
+
+'''
+NEW Course
+is done directly in database, ie no API for this function
+'''
+
+
+'''
+NEW PLAYER
+JSONinput: first_name, last_name, email, hcp, club_id
+JSONoutput: ---
+'''
+@app.route('/newplayer', methods=['POST'])
+def newuser():
+	newplayer=Player(request.json["first_name"], request.json["last_name"],request.json["email"],request.json["hcp"], None, request.json["club_id"])
+	try:
 		db.session.add(newplayer)
 		db.session.commit()
-		return ("Player entered into database")
+		return ("Player entered into database"), 200
 	except exc.IntegrityError:
-		return "integrity error"
+		return "integrity error", 502
 
-	# newclub=Club("Lunds Akademiska golfklubb")
-	# newplayer=Player("Henrik", "Boris-Möller", "henrik.borismoller@gmail.com", 88, 1)
-	# db.session.add(newclub)
-	# db.session.add(newplayer)
 
-	# try:
-	# 	db.session.commit()
-	# 	return "Hej new player and club:)"
-	# except exc.IntegrityError:
-	# 	return "Integrity error. did mail already exist?"
 
+
+'''
+NEW ROUND
+JSONinput: course_id, player_id
+JSONoutput: ---
+'''
+@app.route('/newround', methods=['POST'])
+def newround():
+    newround=Round(request.json['course_id'], request.json['player_id'])
+    try:
+        db.session.add(newround)
+        db.session.commit()
+        return "Round started and added to database", 200
+    except exc.IntegrityError:
+        return "integrity error", 502
+
+
+
+'''
+NEW SCORE
+JSONinput: round_id, player_id
+JSONoutput: ---
+'''
+@app.route('/newscore', methods=['POST'])
+def newscore():
+    newscore=Score(request.json['round_id'], request.json['player_id'])
+    try:
+        db.session.add(newscore)
+        db.session.commit()
+        return "Score added", 200
+    except exc.IntegrityError:
+        return "integrity error", 502
+
+
+
+'''
+NEW SCOREHOLE
+JSONinput: round_id, player_id, hole_id, shots
+JSONoutput: ---------
+'''
+@app.route('/scorehole', methods=['POST'])
+def scorehole():
+    scorehole=ScoreHole(request.json['round_id'], request.json['player_id'], request.json['hole_no'], request.json['shots'])
+    try:
+        db.session.add(scorehole)
+        db.session.commit()
+        return "scorehole added", 200
+    except exc.IntegrityError:
+        return "integrity error", 502
+
+
+
+
+
+
+
+
+#-----------------------------OLD stuff: replace with functions using psql database----------------------------
 
 @app.route('/', methods=['GET'])
 def index():
@@ -160,14 +319,14 @@ def user(userId):
 		return jsonify({"userInfo":henrik})
 	if userId==2:
 		return jsonify({"userInfo":axel})
-	else: 
+	else:
 		return jsonify({"error": {"message":"There exist no user with that id", "type":"not found"}})
 
 @app.route("/scorecard/<int:scoreCardId>", methods=['GET'])
 def scoreCard(scoreCardId):
 	if scoreCardId==1:
 		return jsonify({"scoreCard":card})
-	else: 
+	else:
 		return jsonify({"error": {"message":"There exist no round with that id", "type":"not found"}})
 
 
@@ -180,7 +339,7 @@ def addScore(scoreCardId):
 			return jsonify({"error": {"message":"There exist no hole with that id", "type":"not found"}})
 		card["score"][hole]=score
 		return jsonify({"hole":hole, "score":score})
-	else: 
+	else:
 		return jsonify({"error": {"message":"There exist no round with that id", "type":"not found"}})
 
 
